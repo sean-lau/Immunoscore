5530c9b0f81e8f74dee08e7d1cdf1703b740bf71
sean-lau <100877220+sean-lau@users.noreply.github.com>
2023-02-05 23:29:10 -0600

    Add files via upload

---
+++ randomforest.R
@@ -0,0 +1,107 @@
+##Develop Module Deconvolution Ratio Classifer
+#use randomForest fxn
+install.packages("randomForest")
+library(randomForest)
+
+##load in deconvolution data
+Moduledeconv<-read.csv("~/Desktop/PatelLab/Analysis_Results/Deconvolution/Cibersort_Module_deconvolution.csv")
+
+#load InhouseMeningioma_Clippr_results and bulk sample data
+InhouseMeningioma_CLIPPR_results <- read.csv("~/Desktop/PatelLab/FinalFigureAnalysis/InhouseMeningioma_CLIPPR_results.csv", row.names=1)
+load("~/Desktop/PatelLab/09192022_GE_RAW_Normalized_N330PrimarySamples_GeneSymbol.rda")
+
+
+#load in newpredsamples
+newpred<-InhouseMeningioma_CLIPPR_results[,c(1,7)]
+head(newpred)
+intersecting<-intersect(samples$Sample.Name, row.names(newpred))
+index<-""
+for(x in 1:length(intersecting)){
+  index<-c(index,which(row.names(newpred)==intersecting[x]))
+}
+index<-index[-1]
+index<-as.numeric(index)
+
+newpred<-newpred[-index,]
+
+
+index<-""
+for(x in 1:length(intersecting)){
+  index<-c(index,which(samples$Sample.Name==intersecting[x]))
+}
+index<-index[-1]
+index<-as.numeric(index)
+
+samples<-samples[-index,]
+
+newpred<-newpred[c(-3,-4),]
+newpred$samplename<-samples[1:2,1]
+excludedsamples<-newpred
+##two samples whose names needed manually fixing. The format of the original names was switched. 
+row.names(InhouseMeningioma_CLIPPR_results)[1]<-excludedsamples[1,3]
+row.names(InhouseMeningioma_CLIPPR_results)[2]<-excludedsamples[2,3]
+##After above steps, InhouseMeningioma... should have all overlapping samples. Steps below require the modified version of InhouseMeningioma
+##reload sample data
+load("~/Desktop/PatelLab/09192022_GE_RAW_Normalized_N330PrimarySamples_GeneSymbol.rda")
+#continue analysis
+newpred<-InhouseMeningioma_CLIPPR_results[,c(1,7)]
+samples$rowname<-row.names(samples)
+newpred$ID<-row.names(newpred)
+newpredsamples<-merge(samples,newpred, by.x = "Sample.Name", by.y = "ID")
+row.names(newpredsamples)<-newpredsamples$rowname
+
+#index Moduledeconv with overlapping samples in newpredsamples
+ID<-row.names(newpredsamples) 
+index<-""
+for(x in 1:length(ID)){
+  index<-c(index, which(Moduledeconv[,1]==ID[x]))
+}
+index<-index[-1]
+index<-as.numeric(index)
+Moduledeconv<-Moduledeconv[index,]
+
+row.names(Moduledeconv)<-Moduledeconv[,1]
+Moduledeconv<-Moduledeconv[,-1]
+Moduledeconv$rowname<-row.names((Moduledeconv))
+
+#add new prediction annotations
+sampleclass<-as.data.frame(newpredsamples$pred)
+row.names(sampleclass)<-row.names(newpredsamples)
+sampleclass$ID<-row.names(sampleclass)
+colnames(sampleclass)<-c("TumorClass", "ID")
+
+Moduledeconv<-merge(Moduledeconv,sampleclass, by.x = "rowname", by.y = "ID")
+row.names(Moduledeconv)<-Moduledeconv[,1]
+Moduledeconv<-Moduledeconv[,-1]
+Moduledeconv$TumorClass<-as.factor(Moduledeconv$TumorClass)
+#create train and data matrices
+#train matrix
+trainddf<-newpredsamples[newpredsamples$isTrain=="yes",]
+trainindex<-""
+ID<-row.names(trainddf)
+for(x in 1:length(ID)){
+  trainindex<-c(trainindex, which(row.names(Moduledeconv)==ID[x]))
+}
+trainindex<-trainindex[-1]
+trainindex<-as.numeric(trainindex)
+train<-Moduledeconv[trainindex,]
+train<-as.data.frame(train)
+
+train$TumorClass<-as.factor(train$TumorClass)
+
+#data matrix
+data<-Moduledeconv
+
+#randomforest function
+model <- randomForest(TumorClass~.,  data=train,method='class', ntree = 5000)
+ ## all the samples will be used in prediction
+class<-predict(model, newdata=data.frame(data), type='response')
+prob<-predict(model, newdata=data.frame(data), type='prob')
+#train is a matrix with rows module ids, columns training samples, values are ratios.
+#data is a matrix with rows module id, columns all samples, values are ratios.
+
+TumorClass<-data.frame(newpredsamples$predictions_meta_class, class)
+colnames(TumorClass)<-c("Meta Predictions", "randomForest Class")
+
+write.table(TumorClass, file = "~/Desktop/PatelLab/FinalFigureAnalysis/randomtreeTumorClasspredictions.txt", quote = F, sep = "\t")
+write.table(prob, file = "~/Desktop/PatelLab/FinalFigureAnalysis/randomtreeprobability.txt", quote = F, sep = "\t")
